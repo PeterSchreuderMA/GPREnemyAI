702ee5d438ac6af8284f7a50d8b4650ec4b5e540
PeterSchreuder_Laptop <24441@ma-web.nl>
2019-05-10 14:00:16 +0200

    Added - Entity Behaviour

---
+++ GPREnemyAI/Assets/Scripts/Entity/EntityEnemyBehaviour.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityEnemyBehaviour : EntityEnemy
+{
+    
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="_value"></param>
+    /// <returns></returns>
+    public Vector3 RotateToDirection(Vector3 _value)
+    {
+        return (Quaternion.Euler(_value) * transform.forward);
+    }
+
+    //-------------------------
+    //-----State functions-----
+    //-------------------------
+
+    #region State Functions
+    public override void StateDefault()
+    {
+        if (entityHealthStateCurrent == EntityHealthStates.Dead)
+            SetCurrentState(AIState.Dead);
+    }
+
+    public override void StateIdle()
+    {
+        if (entityHealthStateCurrent == EntityHealthStates.Dead)
+            return;
+
+        if (targetCurrentRange < enemyAttackRange)
+            SetCurrentState(AIState.Attacking);
+        else
+            SetCurrentState(AIState.Following);
+    }
+
+    public override void StateAlerted()
+    {
+
+    }
+
+    public override void StateSearching()
+    {
+
+    }
+
+    public override void StateFollowing()
+    {
+        if (entityHealthStateCurrent == EntityHealthStates.Dead)
+            return;
+
+        if (aiMoveTo.MoveTo(targetObject.transform.position, moveSpeed, targetRangeToAttack))// * 0.6f  
+            SetCurrentState(AIState.Attacking);
+    }
+
+    public override void StateAttacking()
+    {
+        /*if (entityStateCurrent == EntityStates.Dead)
+            return;
+
+        if (targetBehaviour.entityStateCurrent != EntityStates.Dead)
+        {
+            if (targetRangeCurrent <= targetRangeToAttack)
+                enemyAttack.Attack();
+            else
+                SetCurrentState(AIState.Following);
+        }
+        else
+            SetCurrentState(AIState.Idle);*/
+    }
+
+    public override void StateDead2()
+    {
+
+    }
+    #endregion
+
+    /// <summary>
+    /// Checks the distance to an object
+    /// </summary>
+    /// <param name="_position"></param>
+    /// <returns></returns>
+    public float CheckDistanceToObj(Vector3 _position)
+    {
+        float _distance = Vector3.Distance(transform.position, _position);
+
+        return _distance;
+    }
+}
---
+++ GPREnemyAI/Assets/Scripts/Entity/EntityEnemySpotting.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityEnemySpotting : EntityEnemy
+{
+    public AIState aiCurrentState = AIState.Idle;
+
+    public float maxRangeSpotting = 10f;
+    public float timeSearching = 5f;
+
+
+    //--------------------------------------------------------------------------
+
+    /// <summary>
+    /// Returns true if the target is spotted in the fov
+    /// </summary>
+    /// <param name="_fovAngle"></param>
+    /// <returns>bool</returns>
+    private bool Spotting(float _fovAngle)
+    {
+        Vector3 _pos = transform.position;
+
+        bool _spotted = false;
+
+        Vector3 _direction = targetObject.transform.position - _pos;
+        float _angle = Vector3.Angle(_direction, transform.forward);
+
+
+        if (_angle < aiFov * 0.5f)
+        {
+            RaycastHit _hit;
+
+            if (Physics.Raycast(_pos + transform.up, _direction.normalized, out _hit))
+            {
+                if (_hit.collider.gameObject == targetObject)
+                {
+                    _spotted = true;
+                }
+            }
+
+            Debug.DrawRay(transform.position, _direction * maxRangeSpotting, Color.yellow);
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, _direction * maxRangeSpotting, Color.red);
+        }
+
+
+        return _spotted;
+
+    }
+
+
+    //-------------------------
+    //-----State functions-----
+    //-------------------------
+
+    public override void StateIdle()
+    {
+        if (Spotting(aiFov))
+            SetCurrentState(AIState.Alerted);
+    }
+
+    public override void StateAlerted()
+    {
+        if (!Spotting(aiFov))
+            SetCurrentState(AIState.Idle);
+    }
+
+    public override void StateSearching()
+    {
+        AlertedCountDown(timeSearching);
+    }
+
+
+    IEnumerator AlertedCountDown(float _time)
+    {
+        for (int i = 0; i < _time; i++)
+        {
+            if (i == _time)
+                SetCurrentState(AIState.Idle);
+
+            yield return new WaitForSeconds(i);
+        }
+    }
+}
