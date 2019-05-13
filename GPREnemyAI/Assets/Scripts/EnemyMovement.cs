77ef3137bc5c186bdd87194bc0690ce4276693aa
MSI\user <semmyking@gmail.com>
2019-05-13 09:34:58 +0200

    test commit

--- GPREnemyAI/Assets/Scripts/EnemyMovement.cs
+++ GPREnemyAI/Assets/Scripts/EnemyMovement.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine.AI;
 using UnityEngine;
 
-public class EnemyMovment : MonoBehaviour
+public class EnemyMovement : MonoBehaviour
 {
     public Transform enemeyTarget;
     public Transform[] points;
